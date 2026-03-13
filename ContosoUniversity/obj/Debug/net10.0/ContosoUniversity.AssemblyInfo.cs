c567dd350c0aba47027651b41c10d5347ead9168
JeffreyKamangaHDX <kamangajr@hendrix.edu>
2026-03-13 11:51:39 -0500

    Adding I and W to grade enum

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+585b94806d69859cabc3a931ff87fb3d7aaa4dda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433affd84a02eada030ebc14728f384eba45b704")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
