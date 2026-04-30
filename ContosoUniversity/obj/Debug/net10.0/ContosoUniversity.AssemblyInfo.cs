d1abbe9fcc7ee248f06efdd4bbe2a0e00e8ae776
JeffreyKamangaHDX <kamangajr@hendrix.edu>
2026-04-30 14:55:34 -0500

    Step 3 Done

--- ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net10.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18051d3b5d8a9d4d718aa2d8156e851fb114c185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08cbb32c86af36ef98b2b3903eb863953835b414")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
